08c75bf0da6743b751d0c8ccc2669c2cb471444a
FlDanyT <96938294+FlDanyT@users.noreply.github.com>
2025-03-11 18:55:59 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	transactions := []float64{} // [кол-во значений в массиве] {значения через ,}
+
+	for {
+
+		transaction := scanTransaction()
+		if transaction == 0 {
+			break
+		}
+
+		transactions = append(transactions, transaction)
+		
+	}
+
+	balance := calculateBalance(transactions)
+	fmt.Println("Ваш баланс: ", balance)
+
+}
+
+func scanTransaction() float64 {
+
+	var transaction float64
+
+	fmt.Println("Введите транзакцию (n для выхода ): ")
+	fmt.Scan(&transaction)
+
+	return transaction
+
+}
+
+func calculateBalance(transactions []float64) float64{
+	
+	balance := 0.0 // Если 0 то это int
+
+	for _, value := range transactions {
+
+		balance += value
+
+	}
+
+	return balance
+}
